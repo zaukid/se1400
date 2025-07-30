0fb17732c3240f9da77af2b304013030dc219dd7
zaukid <335bay@gmail.com>
2025-07-30 10:36:05 -0600

    final commit

--- milestone6/constants.js
+++ milestone6/constants.js
@@ -1,63 +1,148 @@
+const gameLinks = {
+	"word crush": "browse.html#wordcrushlink",
+	"word": "browse.html#wordcrushlink",
+	"crush": "browse.html#wordcrushlink",
+	"die caster": "browse.html#diecasterlink",
+	"die": "browse.html#diecasterlink",
+	"caster": "browse.html#diecasterlink",
+	"next best": "browse.html#nextbestlink",
+	"next": "browse.html#nextbestlink",
+	"best": "browse.html#nextbestlink",
+	"mystery persona": "browse.html#mysterypersonalink",
+	"mystery": "browse.html#mysterypersonalink",
+	"persona": "browse.html#mysterypersonalink",
+	"short and sweet": "browse.html#shortandsweetlink",
+	"short sweet": "browse.html#shortandsweetlink",
+	"short": "browse.html#shortandsweetlink",
+	"sweet": "browse.html#shortandsweetlink",
+	"two to tango": "browse.html#twototangolink",
+	"two tango": "browse.html#twototangolink",
+	"two": "browse.html#twototangolink",
+	"tango": "browse.html#twototangolink",
+	"snake eyes and box cars": "browse.html#snakeyesandboxcarlink",
+	"snakeeyes and box cars": "browse.html#snakeyesandboxcarlink",
+	"snakeeyes and boxcars": "browse.html#snakeyesandboxcarlink",
+	"snake eyes and boxcars": "browse.html#snakeyesandboxcarlink",
+	"snake eyes & box cars": "browse.html#snakeyesandboxcarlink",
+	"snakeeyes": "browse.html#snakeyesandboxcarlink",
+	"snake eyes": "browse.html#snakeyesandboxcarlink",
+	"boxcars": "browse.html#snakeyesandboxcarlink",
+	"box cars": "browse.html#snakeyesandboxcarlink",
+	"box": "browse.html#snakeyesandboxcarlink",
+	"car": "browse.html#snakeyesandboxcarlink",
+	"cars": "browse.html#snakeyesandboxcarlink",
+	"snake": "browse.html#snakeyesandboxcarlink",
+	"eyes": "browse.html#snakeyesandboxcarlink",
+	"box": "browse.html#snakeyesandboxcarlink",
+	"cars": "browse.html#snakeyesandboxcarlink",
+	"double dice derby": "browse.html#doubledicederbylink",
+	"double dice": "browse.html#doubledicederbylink",
+	"dice derby": "browse.html#doubledicederbylink",
+	"derby dice": "browse.html#doubledicederbylink",
+	"double": "browse.html#doubledicederbylink",
+	"dice": "browse.html#doubledicederbylink",
+	"derby": "browse.html#doubledicederbylink",
+};
+
 const wordcrushtitle = "Word Crush";
-
 const wordcrushbiline = "Brain crippling word recall game";
-
-const wordcrushdetails = "<b>Ages: 8+ Number of Players: 2+ Playing Time: 20+ min</b> Be the first to discard your whole hand to win this brain taxing memory game. Take five minutes to learn the rules and set the game up. Then let the grey matter get its workout in. A good memory is helpful, but to win you'll need to plan ahead and make a few good strategic moves. Impress yourself and your friends by reciting a long list of random words. Or forget a single word and watch your hand grow and your mind shrivel. Players hold word cards in their hands which are played alphabetically as play proceeds. Each time a player adds to the string of previously played cards, he must recite the words on the cards beneath the one he's playing. But hold on! Words are not just alphabetical by their first letter. They can also be played if their word contains a double of the active letter. For example, if F is the active letter, FOGGY can be played, but so can CAFFEINE. Players who fail to correctly recite the word stack add any unrecited words to their own hands. Word Crush outcomes can become unpredictable and frustrating. Best laid plans can be laid to waste in a moment. One forgotten or misremembered word cam mean inevitable defeat. Extraordinarily competitive games can built word lists that cycle through the entire alphabet, and then some. When you win a game of Word Crush, you'll leave feeling like it was all you. Lady Luck had nothing to do with it.";
-
 const wordcrushbrief = "<b>Ages: 8+ Number of Players: 2+ Playing Time: 20+ min</b> Be the first to discard your whole hand to win this brain taxing memory game.";
 
 const twototangotitle = "Two to Tango";
-
 const twototangobiline = "Co-operative play 2-player dice game";
-
-const twototangodetails = "<b>Ages: X Number of Players: Y Playing Time: Z min</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est";
-
 const twototangobrief = "<b>Ages: 10+ Number of Players: 2 Playing Time: 10-20 min</b> Two to Tango is an exhilarating cooperative board game that challenges players to strategically outwit monstrous obstacles through teamwork and intuition.";
 
 const snakeyesandboxcartitle = "Snake Eyes & Box Cars";
-
 const snakeyesandboxcarbiline = "Multiplayer elimination dice game";
-
-const snakeyesandboxcardetails = "<b>Ages: 3+ Number of Players: 2-6 Playing Time: 10 min</b> Roll a 7 when your score is 7, or be the last eliminated to win Snake Eyes & Box Cars. This Yatzee!-style two-dice game pits players against each other in an elimination game of chance. There are only four easy rules to this game that result in scores between -2 & 4 each turn. Players are eliminated when they reach -10. They win by being the sole survivor or by rolling a 7 when their score is 7. Snake Eyes & Box Cars is easy to learn for young children, and is fun for the whole family. Games are quick and entertaining. Game form is small and perfect for travel.";
-
 const snakeyesandboxcarbrief = "<b>Ages: 3+ Number of Players: 2-6 Playing Time: 10 min</b> Roll a 7 when your score is 7, or be the last eliminated to win Snake Eyes & Box Cars.";
 
 const shortandsweettitle = "Short & Sweet";
-
 const shortandsweetbiline = "Ultrafast co-operative dice & card challenge game";
-
-const shortandsweetdetails = "<b>Ages: X Number of Players: Y Playing Time: Z min</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est";
-
 const shortandsweetbrief = "<b>Ages: 8+ Number of Players: 2+ Playing Time: 10-20 min</b> A fast-paced card game where players collect points by gathering candies, cookies, and cupcakes";
 
 const nextbesttitle = "Next Best";
-
 const nextbestbiline = "Know enough about your friends to guess their 2nd favorite thing";
-
-const nextbestdetails = "<b>Ages: X Number of Players: Y Playing Time: Z min</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est";
-
 const nextbestbrief = "<b>Ages: 8+ Number of Players: 3+ Playing Time: 20 min+</b> Invite your friends and family to join you for a thrilling and competitive game night.";
 
 const mysterypersonatitle = "Mystery Persona";
-
 const mysterypersonabiline = "Multiplayer character guessing game with a twist";
-
-const mysterypersonadetails = "<b>Ages: X Number of Players: Y Playing Time: Z min</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est";
-
 const mysterypersonabrief = "<b>Ages: 12+ Number of Players: 3+ Playing Time: 20+ min</b> Mystery Persona is an engaging and interactive guessing game that challenges guessing players to deduce the character chosen by the Picker using a combination of clever questions and strategic clues.";
 
 const diecastertitle = "Die Caster";
-
 const diecasterbiline = "Fast-paced two-die casino Craps-style game";
-
-const diecasterdetails = "<b>Ages: 5+ Number of Players: 2+ Playing Time: 20+ min</b> Hold the highest chip value at the end of the game to win this fast-paced, easy-to-learn game. Die Caster is a casino Craps-style game with none of the complexity, the player elimination or camaraderie. Players are pitted against each other, but all complete against the roller. The rules are simple. After the roller's first two-dice roll, the other players wager whether his second roll will be higher or lower than his first. Winners are always paid what they wagered, and may change their bets right up to the moment the dice are rolled. If the roller throws two equal rolls, he wins instead and gets all the chips on the board. Play continues for a predetermined number of rounds, minutes, or maximum chip value. Die Caster is entertaining but easy to learn for young and old alike. Children are introduced to the concept of two dice roll probability. Trailing players are never truly out until they complete their final rolls. Lead changes can happen rapidly and unexpectedly! Get Die Caster for your game closet, but keep it on top! It takes only a minute or two to refreshen the rules if it's been a while since you last played. It's the perfect game for family gatherings or game nights with friends. Younger children can participate with minimal help from older siblings or parents.";
-
 const diecasterbrief = "<b>Ages: 5+ Number of Players: 2+ Playing Time: 20+ min</b> Hold the highest chip value at the end of the game to win this fast-paced, easy-to-learn game.";
 
 const doubledicederbytitle = "Double Dice Derby";
-
 const doubledicederbybiline = "Multiplayer two-die token domination game";
-
-const doubledicederbydetails = "<b>Ages: X Number of Players: Y Playing Time: Z min</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est";
-
 const doubledicederbybrief = "<b>Ages: 10+ Number of Players: 2-4 Playing Time: 20+ min</b> Experience the thrill of Double Dice Derby, a strategic dice game perfect for family game nights.";
+
+const gameLogosSm = [
+	'wordcrushlogo-sm.png',
+	'diecasterlogo-sm.png',
+	'nextbestlogo-sm.png',
+	'mysterypersonalogo-sm.png',
+	'shortandsweetlogo-sm.png',
+	'snakeyesandboxcarlogo-sm.png',
+	'twototangologo-sm.png',
+	'doubledicederbylogo-sm.png'
+];
+const gameLogosMd = [
+	'wordcrushlogo-md.png',
+	'diecasterlogo-md.png',
+	'nextbestlogo-md.png',
+	'mysterypersonalogo-md.png',
+	'shortandsweetlogo-md.png',
+	'snakeyesandboxcarlogo-md.png',
+	'twototangologo-md.png',
+	'doubledicederbylogo-md.png'
+];
+const gameLogosLg = [
+	'wordcrushlogo-lg.png',
+	'diecasterlogo-lg.png',
+	'nextbestlogo-lg.png',
+	'mysterypersonalogo-lg.png',
+	'shortandsweetlogo-lg.png',
+	'snakeyesandboxcarlogo-lg.png',
+	'twototangologo-lg.png',
+	'doubledicederbylogo-lg.png'
+];
+const Title = [
+	wordcrushtitle,
+	diecastertitle,
+	nextbesttitle,
+	mysterypersonatitle,
+	shortandsweettitle,
+	snakeyesandboxcartitle,
+	twototangotitle,
+	doubledicederbytitle
+];
+const Brief = [
+	wordcrushbrief,
+	diecasterbrief,
+	nextbestbrief,
+	mysterypersonabrief,
+	shortandsweetbrief,
+	snakeyesandboxcarbrief,
+	twototangobrief,
+	doubledicederbybrief
+];
+const Biline = [
+	wordcrushbiline,
+	diecasterbiline,
+	nextbestbiline,
+	mysterypersonabiline,
+	shortandsweetbiline,
+	snakeyesandboxcarbiline,
+	twototangobiline,
+	doubledicederbybiline
+];
+const Link = [
+	'browse.html#wordcrushlink',
+	'browse.html#diecasterlink',
+	'browse.html#nextbestlink',
+	'browse.html#mysterypersonalink',
+	'browse.html#shortandsweetlink',
+	'browse.html#snakeyesandboxcarlink',
+	'browse.html#twototangolink',
+	'browse.html#doubledicederbylink'
+];
